12701144595df43a49ce53da05c6e6ed45a37e23
Artem <mr.aap02@mail.ru>
2024-11-18 12:56:38 +0300

    done

---
+++ CrossplatformDevelopLabs-main/LR1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Popkov.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
